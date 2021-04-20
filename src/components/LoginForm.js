69ae11614a7c869638950e3cc92f63fd00ccf110
cinna-codes <somethingfellipickeditup@gmail.com>
2021-04-20 15:43:20 -0400

    Done.

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -4,22 +4,38 @@ class LoginForm extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      username: '',
+      password: ''
+    };
+  }
+
+  handleChange = event => {
+    this.setState({
+      [event.target.name]: event.target.value // this is reusable code for changing state in controlled forms
+    })
+  }
+
+  verifyLogin = (event) => {
+    event.preventDefault() // submit button default is to refresh the webpage
+    if (!!this.state.username && !!this.state.password) { // verify that neither `username` nor `password` are empty strings
+      this.props.handleLogin(this.state) // handleLogin prop is calling on App.login method, and inputting this(loginForm).state as arguments for it 
+    }
   }
 
   render() {
     return (
-      <form>
+      <form onSubmit={this.verifyLogin}>
         <div>
           <label>
             Username
-            <input id="username" name="username" type="text" />
+            <input id="username" name="username" type="text" value={this.username} onChange={this.handleChange} />
           </label>
         </div>
         <div>
           <label>
             Password
-            <input id="password" name="password" type="password" />
+            <input id="password" name="password" type="password" value={this.password} onChange={this.handleChange} />
           </label>
         </div>
         <div>
--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -4,14 +4,27 @@ class TwitterMessage extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      message: ''
+    };
+  }
+
+  handleMessageChange = (event) => {
+    this.setState({
+      message: event.target.value
+    })
   }
 
   render() {
+    let charCount = this.props.maxChars - this.state.message.length // define this outside of what's actually being rendered; can now call on this in render
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" name="message" id="message" />
+        <input type="text" name="message" id="message" 
+          onChange={this.handleMessageChange}
+          value={this.state.message}
+        />
+        {charCount} 
       </div>
     );
   }
